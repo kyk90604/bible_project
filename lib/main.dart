32118d98da6782bdeace2bb1120b307404d913aa
kyk90604 <kyk90604@gmail.com>
2023-03-25 02:14:03 +0900

    22

--- lib/main.dart
+++ lib/main.dart
@@ -10,44 +10,34 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-
     return MaterialApp(
-
-
-)}
         home: Scaffold(
-          appBar: AppBar(
-              title: Text("성경 검색")
-          ),
-          body: Container( 
-            width: double.infinity, height: 200,
-            child: Row(
-              children: [
-                Container(
-                  padding: EdgeInsets.all(20),
-                  child:Image.asset('test1.png')
-                ),
-                Expanded(
-                  child:Container(
-                      padding: EdgeInsets.fromLTRB(10, 20, 10, 10),
-                      child: Column(
+            appBar: AppBar(title: Text("성경 검색")),
+            body: Container(
+                width: double.infinity,
+                height: 200,
+                child: Row(children: [
+                  Container(
+                      padding: EdgeInsets.all(20),
+                      child: Image.asset('test1.png')),
+                  Expanded(
+                    child: Container(
+                        padding: EdgeInsets.fromLTRB(10, 20, 10, 10),
+                        child: Column(
                             crossAxisAlignment: CrossAxisAlignment.start,
-                            children: [Text("팝니다 노트북 asd \n하나둘셋넷"),
+                            children: [
+                              Text("팝니다 노트북 asd \n하나둘셋넷"),
                               Text("용전동"),
                               Text("40원"),
                               Row(
                                   mainAxisAlignment: MainAxisAlignment.end,
                                   children: [
-                                  Text("aaa"),
-                                  Icon(Icons.heart_broken_sharp),
-                                  Text("11")
-                                ]
-                              )
-                        ]
-                      )
-                    ),
-                )
-              ]
-            )
-        )
-    ));}}
+                                    Text("aaa"),
+                                    Icon(Icons.heart_broken_sharp),
+                                    Text("11")
+                                  ])
+                            ])),
+                  )
+                ]))));
+  }
+}
